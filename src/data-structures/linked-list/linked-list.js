d1698eba7adccc6133b5af6cbe85c0c76795473e
zee-w <zeeshan.wani10@gmail.com>
2024-06-02 20:02:57 +0530

    pop()

--- src/data-structures/linked-list/linked-list.js
+++ src/data-structures/linked-list/linked-list.js
@@ -1,36 +1,57 @@
 import Node from './linkedListNode.js'
 
 
-class LinkedLists{
-    constructor(value){
+class LinkedLists {
+    constructor(value) {
         const newNode = new Node(value);
         this.head = newNode;
         this.tail = this.head;
         this.length = 1;
     }
 
-    push(value){
-        // O(1) operation
+    push(value) {
+        // push is a O(1) operation
         const newNode = new Node(value);
 
         // if the linked list is empty
-        if(!newNode){
+        if (!newNode) {
             this.head = newNode;
-            this.tail =this.head;
-        }else{ //add new node to the existing list
+            this.tail = this.head;
+        } else { //add new node to the existing list
             this.tail.next = newNode;
             this.tail = newNode;
         }
         this.length++; // increase the lenght of the linked list by 1
         return this; // return the updated list.
     }
+
+    pop() {
+        if (this.length === 0) return undefined;
+
+        let temp = this.head;
+        let pre = this.head;
+
+        while (temp.next) {
+            pre = temp;
+            temp = temp.next;
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--
+        if (this.length===0){
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
+    }
 }
 
 
 
 
-const node1 = new LinkedLists(5);
+const linkedList = new LinkedLists(5);
 
-node1.push(6)
-node1.push(7)
-console.log(node1)
+linkedList.push(10);
+linkedList.pop();
+linkedList.pop();
+console.log(linkedList)
